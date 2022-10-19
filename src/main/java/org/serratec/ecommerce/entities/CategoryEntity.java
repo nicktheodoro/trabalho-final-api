7ea091c9533ee2ddfba34ee4826f9adbeb22686a
nicktheodoro <nicolastheodoro@gmail.com>
2022-10-19 17:35:08 -0300

    Criando CategoryEntity

---
+++ src/main/java/org/serratec/ecommerce/entities/CategoryEntity.java
@@ -0,0 +1,21 @@
+package org.serratec.ecommerce.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "category")
+public class CategoryEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "category_id")
+	private Integer categoryId;
+	
+	@Column(name = "category_name", nullable = false)
+	private String categoryName;
+}
